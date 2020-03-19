529ff9223c7c3b83a358de58eddafb4ee61a89d7
Ben Hale <bhale@pivotal.io>
2020-03-19 10:32:08 -0700

    Another attempt
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- scripts/update-dependency.sh
+++ scripts/update-dependency.sh
@@ -18,7 +18,7 @@ version() {
   local PATTERN="([0-9]+)\.?([0-9]*)\.?([0-9]*)(.*)"
 
   if [[ $(cat "${ROOT}"/dependency/version) =~ ${PATTERN} ]]; then
-    echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]:=0}.${BASH_REMATCH[3]:=0}"
+    echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]:-0}.${BASH_REMATCH[3]:-0}"
     return
   else
     echo "version is not semver" 1>&2
